b13d5507431d3e0f718ee53869ce8d9f52697b06
SymplyMatt <symplymatt@gmail.com>
2024-03-11 08:48:24 +0100

    fingerrprint page

--- src/components/auth/fingerprint/FingerPrintForm.tsx
+++ src/components/auth/fingerprint/FingerPrintForm.tsx
@@ -1,10 +1,11 @@
 import FormTitle from "../FormTitle"
+import FingerprintInputSections from "./FingerPrintInputSections"
 
 const FingerprintForm = () => {
   return (
     <div className="form-container">
         <FormTitle title="Create Account" description="Security - Setup Fingerprint"span="(Optional)"/>
-        
+        <FingerprintInputSections />
     </div>
   )
 }
